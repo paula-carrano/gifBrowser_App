c668b3c654b5c6d1a5c99cd48c4e051727d31632
Paula Carrano <paula.agustina.carrano@gmail.com>
2021-06-02 17:01:43 -0300

    responsive

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useState, useContext } from 'react';
-import { Grid, GridList, GridListTile, Typography, InputBase, IconButton } from '@material-ui/core';
+import { Grid, GridList, GridListTile, Typography, InputBase, IconButton, Hidden } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import { CardGif, AlertGif } from './components';
 import { search_Gif } from '../../services/gifSearch';
@@ -40,13 +40,14 @@ const Home: FC = () => {
 
     return (
         <div>
-            <Grid container spacing={2}>
-                <Grid item sm={12} md={11} >
+            <Grid container spacing={1}>
+                <Grid item xs={11} md={11} >
                     <InputBase
                         className="input_search"
                         placeholder="Search your GIF"
                         fullWidth
                         value={searchInput}
+                        name="searInput"
                         onKeyPress={onKeyPressHandler}
                         onChange={(e) => handleInputChange(e)}
                         inputProps={{
@@ -54,28 +55,31 @@ const Home: FC = () => {
                         }}
                     />
                 </Grid>
-                <Grid item sm={12} md={1} >
-                    <IconButton onClick={handleOnClick} aria-label="search">
-                        <SearchIcon />
-                    </IconButton>
-                </Grid>
+                <Hidden only='xs'>
+                    <Grid item xs={1} md={1} >
+                        <IconButton onClick={handleOnClick} aria-label="search">
+                            <SearchIcon />
+                        </IconButton>
+                    </Grid>
+                </Hidden>
             </Grid>
-            <Typography variant="h3" component="h2" gutterBottom>
+            <Typography variant="h5" component="h3" className="principal_title">
                 {titleGif}
             </Typography>
-            <GridList cellHeight={300} cols={5}>
+            <Grid container spacing={1}>
                 {
                     gifSearched ? (
                         gifSearched.map(g => {
                             return (
-                                <GridListTile key={g.id}>
+                                <Grid container item xs={12} md={3} key={g.id}>
                                     <CardGif data={g} />
-                                </GridListTile>
+                                </Grid>
                             )
                         }))
                         : <AlertGif />
                 }
-            </GridList>
+
+            </Grid>
         </div >
     );
 
